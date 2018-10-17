774892acc70a85df692f2cb2b971aed7270084b8
MarwinBl <sergey.solovyov@outlook.com>
2018-10-17 14:24:53 +0300

    фикс ссылки удалить все

--- static/js/basketapp/basket.js
+++ static/js/basketapp/basket.js
@@ -1,7 +1,14 @@
 window.onload = function () {
+    function refresh_del_all(element, value){
+        let href = $(element).attr('href');
+        let list_href = href.split('/');
+        list_href[list_href.length-1] = value;
+        $(element).attr('href', list_href.join('/'));
+    }
     $('.card').on('click', '.action', function () {
         event.preventDefault();
         let elem = event.target;
+        let del_all = $(elem).parents('.card').find('.del-all');
         let url = elem.getAttribute('href');
         $.ajax({
             url: url,
@@ -10,6 +17,7 @@ window.onload = function () {
                 let quantity_block = $('#'+data.name);
                 quantity_block.html(data.quantity);
                 basket_refresh(data.basket);
+                refresh_del_all(del_all, data.quantity);
                 if (data.quantity === 0){
                     quantity_block.parents('.card').remove();
                 }
