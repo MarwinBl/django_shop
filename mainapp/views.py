36d7dc4eb3baf5b141c55812634189ba15f18229
MarwinBl <sergey.solovyov@outlook.com>
2018-11-01 14:39:13 +0300

    добавлены декораторы для передачи контекста с меню

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,13 +4,15 @@ from mainapp.models import *
 from basketapp.views import get_basket
 
 
-def get_menu_context():
-    return {'memu_link': [
-        {'href': 'index', 'name': 'главная', 'submenu': False},
-        {'href': 'category:index', 'name': 'категории', 'submenu': Category.active_objects.all()
-         },
-        {'href': 'contact', 'name': 'контакты', 'submenu': False},
-    ]}
+def menu_decorator(func):
+    def wrapper(request, *args, **kwargs):
+        context_menu = {'memu_link': [
+                        {'href': 'index', 'name': 'главная', 'submenu': False},
+                        {'href': 'category:index', 'name': 'категории', 'submenu': Category.active_objects.all()},
+                        {'href': 'contact', 'name': 'контакты', 'submenu': False},
+                        ]}
+        return func(request, context=context_menu, *args, **kwargs)
+    return wrapper
 
 
 def get_hot_product():
@@ -21,16 +23,16 @@ def get_same_products(product):
     return Product.objects.filter(category__in=product.category.all(), is_active=True).exclude(pk=product.pk)[:3]
 
 
-def index(request):
-    context = get_menu_context()
+@menu_decorator
+def index(request, context):
     context['basket'] = get_basket(request.user)
     context['hot_product'] = get_hot_product()
     context['same_product'] = get_same_products(context['hot_product'])
     return render(request, 'mainapp/index.html', context)
 
 
-def category(request, slug=None, page=1):
-    context = get_menu_context()
+@menu_decorator
+def category(request, context, slug=None, page=1):
     context['basket'] = get_basket(request.user)
     if slug:
         category = get_object_or_404(Category, slug=slug, is_active=True)
@@ -50,15 +52,15 @@ def category(request, slug=None, page=1):
         return render(request, 'mainapp/categories.html', context)
 
 
-def about(request, product):
-    context = get_menu_context()
+@menu_decorator
+def about(request, context, product):
     context['basket'] = get_basket(request.user)
     _product = get_object_or_404(Product, slug=product, is_active=True)
     context['product'] = _product
     return render(request, 'mainapp/about.html', context)
 
 
-def contact(request):
-    context = get_menu_context()
+@menu_decorator
+def contact(request, context):
     context['basket'] = get_basket(request.user)
     return render(request, 'mainapp/contact.html', context)
